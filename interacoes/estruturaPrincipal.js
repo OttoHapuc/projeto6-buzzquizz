37bd9f7a2b301913030d3e6905c6a96610d91994
Ottoniel Hapuc <dossantoshapuc@gmail.com>
2022-11-02 22:45:55 -0300

    listagem de todo os quizzes

---
+++ interacoes/estruturaPrincipal.js
@@ -0,0 +1,39 @@
+
+// Exibição de Quizz tela-1 ===========================
+
+function carregaQuizzDoServidor() {
+
+    const promise = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes');
+
+    promise.then(processaDados);
+}
+carregaQuizzDoServidor();
+
+let lista = []
+function processaDados(resposta){
+    
+    for (let i = 0; i < resposta.data.length; i++) {
+        lista.push(resposta.data[i]);
+    }
+
+    exibeQuizzes();
+}
+
+function exibeQuizzes() {
+
+    const quizzEscolhido = document.querySelector('.corpoDeApresentacao');
+    quizzEscolhido.innerHTML = "";
+
+
+    for (let i = 0; i < lista.length; i++) {
+
+        let divQuizzescolhido = `
+            <div onclick="quizzSelecionado()" class="quizzEscolhido">
+                <img src="${lista[i].image}">
+                <div class="gradienteTodosOsQuizz flex"><p>${lista[i].title}</p></div>
+            </div>
+        `
+
+        quizzEscolhido.innerHTML += divQuizzescolhido;
+    }
+}
