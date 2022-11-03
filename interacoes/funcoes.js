abb49fdb2ef825fc9900645af299c2803dd8d6c8
leonardoyusuke <you@example.comleoyuhirano@gmail.com>
2022-11-03 13:53:37 +0000

    primeiras funcoes do criar

--- interacoes/funcoes.js
+++ interacoes/funcoes.js
@@ -1,8 +1,16 @@
+
+
+
 function criarQizz() {
 
     const tela1 = document.querySelector('.AreaDeExibicaoInicial-Tela1')
 
     tela1.classList.add('invisivel')
+
+    //leonardo    adicionar pagina criar quizz
+    const criarTela1 = document.querySelector(".conteudo1")
+
+    criarTela1.classList.remove("invisivel");
 }
 
 function quizzSelecionado() {
@@ -14,3 +22,17 @@ function quizzSelecionado() {
     tela2.classList.remove('invisivel');
 
 }
+
+// leonardo 
+
+function botao1(){
+    const cond1 = document.querySelector("input.tituloQuizz").value
+    if (cond1.length>16 && cond1.length < 65)   {
+        console.log("deu")
+    }
+    else{
+        alert ("preenchar os dados novamente")
+    }
+}
+
+
