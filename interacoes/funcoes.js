a97eca20b57696b8d0a9bbca3bc4a478f3b61f5e
leonardoyusuke <you@example.comleoyuhirano@gmail.com>
2022-11-03 15:00:07 +0000

    segunda pagina criar

--- interacoes/funcoes.js
+++ interacoes/funcoes.js
@@ -1,4 +1,7 @@
-
+let criarTela1 = document.querySelector(".conteudo1")
+let criarTela2 = document.querySelector(".conteudo2")
+let criarTela3 = document.querySelector(".conteudo3")
+let criarTela4
 
 
 function criarQizz() {
@@ -8,8 +11,6 @@ function criarQizz() {
     tela1.classList.add('invisivel')
 
     //leonardo    adicionar pagina criar quizz
-    const criarTela1 = document.querySelector(".conteudo1")
-
     criarTela1.classList.remove("invisivel");
 }
 
@@ -24,15 +25,27 @@ function quizzSelecionado() {
 }
 
 // leonardo 
-
+//pagina 1
 function botao1(){
     const cond1 = document.querySelector("input.tituloQuizz").value
     if (cond1.length>16 && cond1.length < 65)   {
         console.log("deu")
+        criarTela1.classList.add("invisivel");
+        criarTela2.classList.remove("invisivel");
     }
     else{
         alert ("preenchar os dados novamente")
     }
 }
+//pagina 2
+function botao2(){
+    const cond2 = document.querySelector("input.tituloQuizz2").value
+    if (cond2.length>20 ) {
+        criarTela2.classList.add("invisivel");
+        criarTela3.classList.remove("invisivel");
+    }   
+    else{
+        alert ("preenchar os dados novamente")
+    }
 
-
+}
