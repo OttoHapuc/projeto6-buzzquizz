0f750e33a017720acbde99fdd3c8120f8d1a13da
Ottoniel Hapuc <dossantoshapuc@gmail.com>
2022-11-05 19:46:35 -0300

    criação do objt com Leo, quase funcionando

--- interacoes/funcoes.js
+++ interacoes/funcoes.js
@@ -32,38 +32,125 @@ function quizzSelecionado() {
 
 // leonardo 
 //pagina 1
+let tituloDoQuizz = {
+    id:"",
+    title:"",
+    image:"",
+    questions: [],
+    levels: []
+};
+let qntPerguntas = 0;
+let qntNiveis = 0;
 function botao1(event){
     event.preventDefault()
         criarTela1.classList.add("invisivel");
         criarTela2.classList.remove("invisivel");
-        title = document.querySelector("input.tituloQuizz").value
-        image = document.querySelector("input.urlQuizz").value
-        console.log(title)
-        console.log(image)
+        title = document.querySelector("input.tituloQuizz").value;
+        image = document.querySelector("input.urlQuizz").value;
+
+        qntPerguntas = document.querySelector('input.quantidadePergunta').value;
+        qntNiveis = document.querySelector('input.quantidadeNivel').value;
+
+        tituloDoQuizz.title = title;
+        tituloDoQuizz.image = image
+
+        console.log(tituloDoQuizz)
         renderizar()
 }
 //pagina 2
 function renderizar(){  //perguntas
     let adicionarMsg = document.querySelector(".add")
     numeroPergunta = document.querySelector("input.quantidadePergunta").value
-    for( i=0 ; i<numeroPergunta ; i++)
+    for( i=0 ; i<numeroPergunta ; i++){
     adicionarMsg.innerHTML +=`
-            <p class="indicacao indicacao2">Pergunta ${i+1}  <ion-icon onclick="switchAba(this)" name="albums-sharp"></ion-icon></p>
-            <input type="text" minlength="20" class="texto2 reduzir tituloQuizz2" placeholder="Texto da pergunta" required></input> 
-            <input type="color" class="texto2 reduzir corQuizz" placeholder="Cor de fundo da pergunta" required></input> 
-            <p class="indicacao reduzir ">Resposta correta</p>
-            <input type="text"  class="texto2 reduzir respostaCerta" placeholder="Resposta correta"required></input> 
-            <input type="url"  class="texto2 reduzir url0" placeholder="URL da imagem"required></input> 
-            <p class="indicacao reduzir ">Respostas incorretas</p>
-            <input type="text"  class="texto2 reduzir respostaErrada1" placeholder="Resposta incorreta 1"required></input> 
-            <input type="url" pattern="https?://.+" class="texto2 reduzir url1" placeholder="URL da imagem 1"required></input> 
-            <input type="text"  class="texto2 reduzir respostaErrada2" placeholder="Resposta incorreta 2"></input> 
-            <input type="url" pattern="https?://.+" class="texto2 reduzir url2" placeholder="URL da imagem 2"></input> 
-            <input type="text"  class="texto2 reduzir respostaErrada3" placeholder="Resposta correta 3"></input> 
-            <input type="url" pattern="https?://.+" class="texto2 reduzir url3" placeholder="URL da imagem 3"></input> `
+            <div class="pergunta${i+1} inserir2">
+                <p class="indicacao indicacao2"><span>Pergunta ${i+1}</span>  
+                <ion-icon onclick="salvarValoresDaPergunta(this)" name="create"></ion-icon>
+                <input type="text" minlength="20" class="texto2 reduzir tituloQuizz2" placeholder="Texto da pergunta" required></input>
+                <input type="color" class="texto2 reduzir corQuizz" placeholder="Cor de fundo da pergunta" required></input>
+                </p>
+                <p class="indicacao reduzir ">Resposta correta</p>
+                <input type="text"  class="texto2 reduzir respostaCerta" placeholder="Resposta correta"required></input>
+                <input type="url"  class="texto2 reduzir url0" placeholder="URL da imagem"required></input>
+                <p class="indicacao reduzir ">Respostas incorretas</p>
+                <input type="text"  class="texto2 reduzir respostaErrada1" placeholder="Resposta incorreta 1"required></input>
+                <input type="url" pattern="https?://.+" class="texto2 reduzir url1" placeholder="URL da imagem 1"required></input>
+                <input type="text"  class="texto2 reduzir respostaErrada2" placeholder="Resposta incorreta 2"></input>
+                <input type="url" pattern="https?://.+" class="texto2 reduzir url2" placeholder="URL da imagem 2"></input>
+                <input type="text"  class="texto2 reduzir respostaErrada3" placeholder="Resposta correta 3"></input>
+                <input type="url" pattern="https?://.+" class="texto2 reduzir url3" placeholder="URL da imagem 3"></input>
+            </div> `
+    }
+    adicionarMsg.innerHTML += `<button type="submit"  onclick="salvarValoresDaPergunta()" class="botao1">Prosseguir pra criar níveis</button>`
 
-    adicionarMsg.innerHTML += `<button type="submit"  class="botao1">Prosseguir pra criar níveis</button>`
+}
 
+let CorpoPerguntasDoQuizz_questions = {
+    title:"",
+    color:"",
+    answers:[]
+};
+let perguntasDoQuizz_answers = {
+    text: "",
+    image: "",
+    isCorrectAnswer: ""
+};
+function salvarValoresDaPergunta(aDiv) {
+
+    
+    let perguntasDoQuizz_answers = {
+        text: "",
+        image: "",
+        isCorrectAnswer: ""
+    };
+    //chamar os input da aba 2 de criação;
+    //pergunta
+    let textoDaPergunta = document.querySelector('input.tituloQuizz2').value;
+    let corQuizz = document.querySelector('input.corQuizz').value;
+
+    CorpoPerguntasDoQuizz_questions.title = textoDaPergunta;
+    CorpoPerguntasDoQuizz_questions.color = corQuizz;
+
+    let inputRespostacerta = document.querySelector('input.respostaCerta').value;
+    let url0 = document.querySelector('input.url0').value;
+
+    perguntasDoQuizz_answers.text = inputRespostacerta;
+    perguntasDoQuizz_answers.image = url0;
+    perguntasDoQuizz_answers.isCorrectAnswer = true;
+    CorpoPerguntasDoQuizz_questions.answers.push(perguntasDoQuizz_answers)
+
+    let respostaErrada1 = document.querySelector('input.respostaErrada1').value;
+    let url1 = document.querySelector('input.url1').value;
+
+    perguntasDoQuizz_answers.text = respostaErrada1;
+    perguntasDoQuizz_answers.image = url1;
+    perguntasDoQuizz_answers.isCorrectAnswer = false;
+    CorpoPerguntasDoQuizz_questions.answers.push(perguntasDoQuizz_answers)    
+
+    let respostaErrada2 = document.querySelector('input.respostaErrada2').value;
+    let url2 = document.querySelector('input.url2').value;
+
+    perguntasDoQuizz_answers.text = respostaErrada2;
+    perguntasDoQuizz_answers.image = url2;
+    perguntasDoQuizz_answers.isCorrectAnswer = false;
+    CorpoPerguntasDoQuizz_questions.answers.push(perguntasDoQuizz_answers) 
+
+    let respostaErrada3 = document.querySelector('input.respostaErrada3').value;
+    let url3 = document.querySelector('input.url3').value;
+
+    perguntasDoQuizz_answers.text = respostaErrada3;
+    perguntasDoQuizz_answers.image = url3;
+    perguntasDoQuizz_answers.isCorrectAnswer = false;
+    CorpoPerguntasDoQuizz_questions.answers.push(perguntasDoQuizz_answers) 
+
+    tituloDoQuizz.questions.push(CorpoPerguntasDoQuizz_questions);
+
+    const batata = aDiv.parentNode.parentNode;
+    batata.innerHTML = `
+        <p class="indicacao indicacao2"><span>Pergunta ${i+1}</span>  
+            <ion-icon style="color: green;" name="create"></ion-icon>
+        </p>
+    `
 }
 // TELA 3
 function botao2(event){
@@ -112,4 +199,15 @@ function botao3(event){
         adicionarTitulo.innerHTML += title
  
     
+}
+
+let armazenamentoDeQuizz = [];
+
+
+
+let niveisDoJogo = {
+    title:"",
+    image:"",
+    texto:"",
+    minValue:""
 }
