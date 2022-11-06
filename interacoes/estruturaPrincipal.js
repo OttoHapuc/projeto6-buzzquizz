b356848883e48872752245204244d7bc856e9037
Ottoniel Hapuc <dossantoshapuc@gmail.com>
2022-11-06 20:25:02 -0300

    ids do localStorage sendo exibidos na tela de exibição quando do usuário e retirando a aba de criação grande.

--- interacoes/estruturaPrincipal.js
+++ interacoes/estruturaPrincipal.js
@@ -24,7 +24,58 @@ function exibeQuizzes() {
     const quizzEscolhido = document.querySelector('.corpoDeApresentacao');
     quizzEscolhido.innerHTML = "";
 
+    const exibeQuizzDoUsuario = document.querySelector(".corpoDeApresentacaoUsuario");
+    exibeQuizzDoUsuario.innerHTML = "";
 
+    let dadosDoLocalStorage = localStorage.id;
+    if (localStorage.length !==0){
+        if (localStorage.id.includes(",")){
+
+            const forever = document.querySelector(".botao");
+            forever.classList.add("invisivel");
+
+            const tituloMeusQuses = document.querySelector('.meusQuises');
+            tituloMeusQuses.classList.remove("invisivel");
+
+            let elementosDoLocalStorage = dadosDoLocalStorage.split(",");
+            
+            for (i=0; i< elementosDoLocalStorage.length; i++){
+                for (k=0; k < lista.length; k++){
+                    if (Number(elementosDoLocalStorage[i])=== lista[k].id){
+                        let divQuizzescolhido = `
+                            <div onclick="goToQuizz('${lista[k].id}')" class="quizzEscolhido btn">
+                                <img src="${lista[k].image}">
+                                <div class="gradienteTodosOsQuizz flex"><p>${lista[k].title}</p></div>
+                            </div>
+                        `
+                        exibeQuizzDoUsuario.innerHTML += divQuizzescolhido
+                    }
+                }
+            }
+        }
+    else {
+        const forever = document.querySelector(".botao");
+            forever.classList.add("invisivel");
+
+        const tituloMeusQuses = document.querySelector('.meusQuises');
+        tituloMeusQuses.classList.remove("invisivel");
+
+        let elementosDoLocalStorage = dadosDoLocalStorage.split(",");
+        for (i=0; i< elementosDoLocalStorage.length; i++){
+            for (k=0; k < lista.length; k++){
+                if (Number(elementosDoLocalStorage[i])=== lista[k].id){
+                    let divQuizzescolhido = `
+                        <div onclick="goToQuizz('${lista[k].id}')" class="quizzEscolhido btn">
+                            <img src="${lista[k].image}">
+                            <div class="gradienteTodosOsQuizz flex"><p>${lista[k].title}</p></div>
+                        </div>
+                    `
+                    exibeQuizzDoUsuario.innerHTML += divQuizzescolhido
+                }
+            }
+        }
+    }
+    }
     for (let i = 0; i < lista.length; i++) {
 
         let divQuizzescolhido = `
